0d26ec3436c9c546b0aeb03af0fa4e66d34abb79
Prateek Chaudhary <prateekchaudhary@Prateeks-MacBook-Pro.local>
2025-12-16 22:40:25 +0530

    Use Redis operator

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
-
+using StackExchange.Redis;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
@@ -22,11 +22,11 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.UseStaticFiles();
+
 
 app.MapControllers();
 
-app.MapGet("/api/hello", () => Results.Json(new { message = "Prateek new changes" }));
+app.MapGet("/api/hello", () => Results.Json(new { message = "Prateek new changeweewewweews" }));
 
 app.MapGet("/config", (IConfiguration config) =>
 {
@@ -37,8 +37,31 @@ app.MapGet("/config", (IConfiguration config) =>
         ApiKey = config["ApiKey"]
     };
 });
+
+
+
+app.MapGet("/api/redis-test", async (IConfiguration config) =>
+{
+    var host = config["REDIS_HOST"];
+    var port = config["REDIS_PORT"];
+
+    try
+    {
+        var redis = await ConnectionMultiplexer.ConnectAsync($"{host}:{port}");
+        var db = redis.GetDatabase();
+
+        await db.StringSetAsync("operator:test", "Hello Operator");
+        var value = await db.StringGetAsync("operator:test");
+
+        return Results.Ok(new { value = value.ToString() });
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
 //commentss
 // Serve the SPA/default page for any non-API route
-app.MapFallbackToFile("index.html");
-
+app.UseStaticFiles();
 app.Run("http://0.0.0.0:8080");
